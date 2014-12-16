14f1a92e4530dcc2dc4b4f1cf2fa77907c5aeaca
Adam Conkey <adam.conkey@gmail.com>
2014-12-16 10:31:37 -0600

    Update gd.m
    Add error calculation and return of error value. Add further comments. Change variable names.

--- gradient_descent/gd.m
+++ gradient_descent/gd.m
@@ -1,22 +1,26 @@
-function [] = gd(Input,Target,eta,n_epoch)
+function [err] = gd(Inputs,Targets,eta,n_epoch)
 
-w_max = 0.1;
-n_inst = size(Input,1);
-n_in = size(Input,2);
+w_max = 0.1; 	% initial weights in range [-w_max,w_max]
+n_inst = size(Inputs,1);
+n_in = size(Inputs,2);
 
 % concatenate a column for bias node:
-Input = [ones(n_inst,1) Input];
+Inputs = [ones(n_inst,1) Inputs];
 
+% initialize random weights:
 W = (2*w_max).*rand(n_in,1)-w_max;
 dW = zeros(size(W));
+err = 0;
 
 % train for specified epochs:
 for i = 1:n_epoch
-	% iterate over each training instance
+	% iterate over each training instance:
 	for d = 1:n_inst
 		x = Input(d,:);
-		out = x*W;
-		dW = dW + eta.*(Target(d,:)-out).*x;
+		targ_d = Targets(d,:);
+		out_d = x*W;
+		dW = dW + eta.*(targ_d-out_d).*x;
+		err = err + 0.5*(targ_d-out_d)^2
 	end
 	W = W + dW;
 end
