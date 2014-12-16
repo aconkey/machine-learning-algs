9abd0fcd4e0777837a7e81e11668bee7860bf811
Adam Conkey <adam.conkey@gmail.com>
2014-12-16 10:36:58 -0600

    Update gd_stoch.m
    Add error calculation and return error value. Change variable names.

--- gradient_descent/gd_stoch.m
+++ gradient_descent/gd_stoch.m
@@ -1,20 +1,24 @@
-function [] = gd_stoch(Input,Target,eta,n_epoch)
+function [err] = gd_stoch(Inputs,Targets,eta,n_epoch)
 
-w_max = 0.1;
-n_inst = size(Input,1);
-n_in = size(Input,2);
+w_max = 0.1; 	% weights initialized in [-w_max,w_max]
+n_inst = size(Inputs,1);
+n_in = size(Inputs,2);
 
 % concatenate a column for bias node:
-Input = [ones(n_inst,1) Input];
+Inputs = [ones(n_inst,1) Inputs];
 
+% initialize small random weights
 W = (2*w_max).*rand(n_in,1)-w_max;
 
 % train for specified epochs:
 for i = 1:n_epoch
+	err = 0;
 	% iterate over each training instance
 	for d = 1:n_inst
-		x = Input(d,:);
-		out = x*W;
-		W = W + eta.*(Target(d,:)-out).*x;
+		x = Inputs(d,:);
+		targ_d = Targets(d,:);
+		out_d = x*W;
+		W = W + eta.*(targ_d-out_d).*x;
+		err = err + 0.5*(targ_d-out_d)^2;
 	end
 end
