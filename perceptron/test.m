dfe02311d972e9151dd4a2622d5a9961796c4c0b
Adam Conkey <adam.conkey@gmail.com>
2014-12-16 19:26:39 -0600

    Delete test.m

--- perceptron/test.m
+++
@@ -1,6 +0,0 @@
-function [perc_correct] = test_perceptron(filename, n_in)
-	data = csvread(filename);
-	Inputs = data(:,:n_in);
-	Targets = data(:,n_in+1:);
-	perc_correct = perceptron(Inputs,Targets,0.1,100)
-end
